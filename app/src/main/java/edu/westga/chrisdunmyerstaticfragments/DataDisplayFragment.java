205350ad10470acc7d770cfc9fec688f5b2eb70a
Chris Dunmyer <cdunmye1@my.westga.edu>
2016-02-10 19:04:51 -0500

    Updating DataDisplayFragment to have code that will be used by MainActivity.

--- app/src/main/java/edu/westga/chrisdunmyerstaticfragments/DataDisplayFragment.java
+++ app/src/main/java/edu/westga/chrisdunmyerstaticfragments/DataDisplayFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -13,17 +14,38 @@ import android.view.ViewGroup;
  */
 public class DataDisplayFragment extends Fragment {
 
+    private TextView resultTextView;
+    private double number1Text;
+    private double number2Text;
+    private double productResult;
 
     public DataDisplayFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data_display, container, false);
+        View theView = inflater.inflate(R.layout.fragment_data_display, container, false);
+        this.resultTextView = (TextView) theView.findViewById(R.id.resultTextView);
+        return theView;
+    }
+
+    public void multiply(double number1, double number2) {
+        this.productResult = number1 * number2;
+    }
+
+    public void displayProduct() {
+        resultTextView.setText(Double.toString(this.productResult));
+    }
+
+    public void setNumber1Text(double number1Text) {
+        this.number1Text = number1Text;
+    }
+
+    public void setNumber2Text(double number2Text) {
+        this.number2Text = number2Text;
     }
 
 }
